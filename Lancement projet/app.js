49088c7960e820cfb17c4c2d15de151342091c05
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-02-16 15:48:06 +0100

    Menu dropdown

--- Lancement projet/app.js
+++ Lancement projet/app.js
@@ -1,24 +1,94 @@
-const Photos = [
-    {image:"media/portrait.png",
-     titre: "Photo Portrait", 
-     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
+const article = [
+    {   id : 1,
+        image:"media/portrait.png",
+        titre: "Photo Portrait",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
     },
 
-    {image: "media/paysage.png",
-     titre: "Photo Paysage",
-     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
+    {   id : 2,
+        image: "media/paysage.png",
+        titre: "Photo Paysage",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
     },
 
-    {image: "media/poire.png",
-     titre: "Photo Artistique",
-     description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
+    {   id : 3,
+        image: "media/poire.png",
+        titre: "Photo Artistique",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
     }
 ];
 
+function createdCard(article) {
+    const cards = document.getElementById("cards");
+    cards.classList.add("photos");
 
+    const card = document.createElement("div");
+    cards.appendChild(card);
+    card.classList.add("card");
+    card.classList.add("photo");
+
+
+    const h3 = document.createElement("titre");
+    h3.textContent = article.titre;
+    card.appendChild(h3);
+    h3.classList = "titre_photo";
+
+    const img = document.createElement("img");
+    img.src=article.image;
+    img.textContent = article.image;
+    card.appendChild(img);
+    img.classList = "photos";
+
+    const p = document.createElement("texte");
+    p.textContent = article.description;
+    card.appendChild(p);
+    p.classList = "description";
+
+}
+
+
+window.addEventListener('load', function () {
+    const container = document.getElementById("feed-container");
+    article.forEach((article) => { createdCard(article, container) });
+});
+
+async function fetchArticles() {
+
+    const reponse = await fetch("https://v2.jokeapi.dev/joke/Any?lang=fr&amount=10");
+    const jokeapi = await reponse.json();
+    console.log(jokeapi);
+
+}
+
+
+
+
+//DROPDOWN
 function dropdown() {
     document.getElementById("dropdownMenu").classList.toggle("show");
 }
+
+//ferme le dropdown quand on click en dehors de celui-ci
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+//FORMULAIRE DYNAMIQUE
+
+
+
+
+
 
 
 
