b45fcb4cf804df91967a4380998a81e0dd2d6756
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-02-16 10:15:55 +0100

    Ajout projet

---
+++ Lancement projet/app.js
@@ -0,0 +1,24 @@
+const Photos = [
+    {image:"media/portrait.png",
+     titre: "Photo Portrait", 
+     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
+    },
+
+    {image: "media/paysage.png",
+     titre: "Photo Paysage",
+     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
+    },
+
+    {image: "media/poire.png",
+     titre: "Photo Artistique",
+     description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero, consectetur non a fugiat"
+    }
+];
+
+
+
+
+
+
+
+
